3cee5295c31a21e60da74d9b5b4cc154a2446385
王一杨 <wangyiyang@163.com>
2019-03-11 11:41:42 +0800

    更新@vue/cli

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production' ? './' : '/', //设置output.publicPath，区分生产环境和开发环境
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/', //设置output.publicPath，区分生产环境和开发环境
     outputDir: 'dist', //生成的生产环境构建文件的目录,默认dist文件名
     assetsDir: 'static', //配置静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
     productionSourceMap: false, //生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
